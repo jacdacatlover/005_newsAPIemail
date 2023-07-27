ee37dfff5490b0b7cac3c2ec583c6171e7f65ec0
jacdacatlover <jacdacatlover@gmail.com>
2023-07-28 01:40:31 +0800

    Day28- Add links, specify language, limit news, add subject, control category

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import requests
 from send_email import send_email
 
+topic = "generative AI"
+
 api_key = "cda2b821ef5e4c6da765b67791032118"
-url = "https://newsapi.org/v2/everything?q=AI&" \
-    "from=2023-07-26&to=2023-07-26&sortBy=popularity&apiKey="\
-    "cda2b821ef5e4c6da765b67791032118"
+url = "https://newsapi.org/v2/everything?" \
+      f"q={topic}&" \
+      "sortBy=publishedAt&"\
+      "apiKey=cda2b821ef5e4c6da765b67791032118&"\
+      "language=en"
 
 #made request here
 request = requests.get(url)
@@ -14,11 +18,15 @@ content = request.json()
 
 #access the article tile and descriptions
 body = ""
-for article in content["articles"]:
-    body = body + article['title'] + "\n" + article['description'] +2*"\n"
+for article in content["articles"][:20]:
+    if article["title"] is not None:
+        body = body + article['title'] + "\n" \
+               + article['description'] \
+               +"\n" + article['url'] + 2*"\n"
 
+body="Subject: Today AI News" + "\n"+body
 body = body.encode("utf-8")
-send_email(body)
+send_email(message=body)
 
 
 
