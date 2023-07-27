43d7c347eee8390e0c4fbf84af742b087d4d29fb
jacdacatlover <jacdacatlover@gmail.com>
2023-07-27 21:03:36 +0800

    Day27-Send an email with news title and description

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from send_email import send_email
 
 api_key = "cda2b821ef5e4c6da765b67791032118"
 url = "https://newsapi.org/v2/everything?q=AI&" \
@@ -12,10 +13,12 @@ request = requests.get(url)
 content = request.json()
 
 #access the article tile and descriptions
+body = ""
 for article in content["articles"]:
-    print(article['title'])
-    print(article['description'])
+    body = body + article['title'] + "\n" + article['description'] +2*"\n"
 
+body = body.encode("utf-8")
+send_email(body)
 
 
 
